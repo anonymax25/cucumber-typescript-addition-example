684c654a495e243e15a683d4fb9295925a7ce521
Maxime <maxime.dharboulle@gmail.com>
2021-12-09 16:09:11 +0100

    fix

--- test/test.js
+++ test/test.js
@@ -1,17 +1,10 @@
 
-const Wallet = require('../build/index').Wallet
-const Currencies = require('../build/index').Currencies
-const Stock = require('../build/index').Stock
-
+const Adder = require('../build/index').Adder
 const assert =  require('chai').assert;
 
-describe('Test build index', function() {
-  it('should return the value of the US dollar and Roubbles wallet in Euros', async function() {
-      const baseCurrency = Currencies.EUR
-      const wallet = new Wallet()
-      wallet.stocks.push(new Stock(Currencies.USD, 34))
-      wallet.stocks.push(new Stock(Currencies.RUB, 3200))
-      const result = await Wallet.computeValue(wallet, baseCurrency, '2021-01-17')
-      assert.strictEqual(result, 64.13);
+describe('Test Build Adder', function() {
+  it('should return the sum', async function() {
+      const result = Adder.add(1, 2)
+      assert.strictEqual(result, 3);
   });
 });
